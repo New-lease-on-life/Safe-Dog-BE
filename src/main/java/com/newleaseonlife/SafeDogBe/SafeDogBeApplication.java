2e841be13570e6a6a59cda9f62c37713ac58c332
taeju <tjxown0@gmail.com>
2026-02-11 10:54:16 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/com/newleaseonlife/SafeDogBe/SafeDogBeApplication.java
@@ -0,0 +1,13 @@
+package com.newleaseonlife.SafeDogBe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeDogBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeDogBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/newleaseonlife/SafeDogBe/SafeDogBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newleaseonlife.SafeDogBe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeDogBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
