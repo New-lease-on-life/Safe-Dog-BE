3ab8afe3cd3014d33ec274021d0d5c6f6194e9ec
taeju <tjxown0@gmail.com>
2026-03-06 15:07:48 +0900

    [feat]: aws s3 도메인 수정

--- src/main/java/com/newleaseonlife/SafeDogBe/global/config/S3Config.java
+++ src/main/java/com/newleaseonlife/SafeDogBe/global/config/S3Config.java
@@ -1,8 +1,6 @@
 package com.newleaseonlife.SafeDogBe.global.config;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
@@ -13,24 +11,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class S3Config {
 
-  @Value("${cloud.aws.credentials.access-key}")
-  private String accessKey;
-
-  @Value("${cloud.aws.credentials.secret-key}")
-  private String secretKey;
-
   @Value("${cloud.aws.region.static}")
   private String region;
 
   @Bean
   public AmazonS3 amazonS3Client() {
-    // 1. AWS 자격 증명(Credentials) 객체 생성
-    AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-
-    // 2. S3 클라이언트 빌더를 통해 Region과 자격 증명을 엮어서 AmazonS3 객체 반환
+    // 백엔드 보안 핵심: AWS 자격 증명 탐색 체인 적용 (OIDC, EC2 IAM Role 자동 인식)
     return AmazonS3ClientBuilder
         .standard()
-        .withCredentials(new AWSStaticCredentialsProvider(credentials))
+        .withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
         .withRegion(region)
         .build();
   }
